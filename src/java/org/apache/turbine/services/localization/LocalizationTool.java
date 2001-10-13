06f5267345d5030aeeb0c0e6a5abb70e748ca6eb
Daniel Rall <dlr@apache.org>
2001-10-13 23:22:52 +0000

    Easy to use localization pull tool from Turbine 3.
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/turbine/core/trunk@220458 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/turbine/services/localization/LocalizationTool.java
@@ -0,0 +1,170 @@
+package org.apache.turbine.services.localization;
+
+/* ====================================================================
+ * The Apache Software License, Version 1.1
+ *
+ * Copyright (c) 2001 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution,
+ *    if any, must include the following acknowledgment:
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowledgment may appear in the software itself,
+ *    if and wherever such third-party acknowledgments normally appear.
+ *
+ * 4. The names "Apache" and "Apache Software Foundation" and
+ *    "Apache Turbine" must not be used to endorse or promote products
+ *    derived from this software without prior written permission. For
+ *    written permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache",
+ *    "Apache Turbine", nor may "Apache" appear in their name, without
+ *    prior written permission of the Apache Software Foundation.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ */
+
+import java.util.Locale;
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
+
+import org.apache.turbine.services.localization.Localization;
+import org.apache.turbine.services.pull.ApplicationTool;
+import org.apache.turbine.util.RunData;
+
+/**
+ * A pull tool which provides lookups for localized text by delegating
+ * to the configured <code>LocalizationService</code>.
+ *
+ * @author <a href="mailto:dlr@collab.net">Daniel Rall</a>
+ * @author <a href="mailto:jon@collab.net">Jon Stevens</a>
+ */
+public class LocalizationTool implements ApplicationTool
+{
+    /**
+     * The language and country information parsed from the request's
+     * <code>Accept-Language</code> header.  Reset on each request.
+     */
+    protected Locale locale;
+
+    /**
+     * The bundle for this request.
+     */
+    private ResourceBundle bundle;
+
+    /**
+     * The name of the bundle for this tool to use.
+     */
+    private String bundleName;
+
+    /**
+     * Creates a new instance.  Used by <code>PullService</code>.
+     */
+    public LocalizationTool()
+    {
+        refresh();
+    }
+
+    /**
+     * <p>Performs text lookups for localization.</p>
+     *
+     * <p>Assuming there is a instance of this class with a HTTP
+     * request set in your template's context named <code>l10n</code>,
+     * the VTL <code>$l10n.HELLO</code> would render to
+     * <code>hello</code> for English requests and <code>hola</code>
+     * in Spanish (depending on the value of the HTTP request's
+     * <code>Accept-Language</code> header).</p>
+     *
+     * @param key The identifier for the localized text to retrieve.
+     * @return The localized text.
+     */
+    public String get(String key)
+    {
+        if (bundle == null)
+        {
+            // Cache bundle for use during current request.
+            bundle = Localization.getBundle(bundleName, locale);
+        }
+
+        try
+        {
+            return bundle.getString(key);
+        }
+        catch (MissingResourceException noKey)
+        {
+            return null;
+        }
+    }
+
+    /**
+     * The return value of this method is used to set the name of the
+     * bundle used by this tool.  Useful as a hook for using a
+     * different bundle than specifed in your
+     * <code>LocalizationService</code> configuration.
+     *
+     * @param data The inputs passed from {@link #init(Object)}.
+     * (ignored by this implementation).
+     */
+    protected String getBundleName(Object data)
+    {
+        return Localization.getDefaultBundleName();
+    }
+
+
+    // ApplicationTool implmentation
+
+    /**
+     * Sets the request to get the <code>Accept-Language</code> header
+     * from (reset on each request).
+     */
+    public final void init(Object data)
+    {
+        if (data instanceof RunData)
+        {
+            // Pull necessary information out of RunData while we have
+            // a reference to it.
+            locale = Localization.getLocale( ((RunData) data).getRequest() );
+            bundleName = getBundleName(data);
+        }
+    }
+
+    /**
+     * No-op.
+     */
+    public void refresh()
+    {
+        locale = null;
+        bundle = null;
+        bundleName = null;
+    }
+}
