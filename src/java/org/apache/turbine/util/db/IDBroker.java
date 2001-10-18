e7691707fc13e1645245b149ea0849a2b6c66585
Daniel Rall <dlr@apache.org>
2001-10-18 22:00:31 +0000

    Added some more great text by John McNally <jmcnally@collab.net> to help further clarify the IDBroker's warning messages for databases--like MySQL--which do not support transactions (out of the box).
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/turbine/core/trunk@220474 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/turbine/util/db/IDBroker.java
+++ src/java/org/apache/turbine/util/db/IDBroker.java
@@ -234,10 +234,13 @@ public class IDBroker
         }
         if (!transactionsSupported)
         {
-            Log.warn("IDBroker is being used with db: " + dbName +
-                "\n which does not support transactions.  It is possible to " +
-                "\n generate duplicate keys, if multiple JVM's are used or other " +
-                "\n means are used to write to the database.");
+            Log.warn
+                ("IDBroker is being used with db '" + dbName +
+                 "', which does not support transactions.  IDBroker " +
+                 "attempts to use transactions to limit the possibility of " +
+                 "duplicate key generation.  Without transactions, " +
+                 "duplicate key generation is possible if multiple JVMs " +
+                 "are used or other means are used to write to the database.");
         }
     }
 
