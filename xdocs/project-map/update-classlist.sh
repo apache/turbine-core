b28578415dfa5e8c662370c2e87c378e9bd10e95
mpoeschl <mpoeschl@unknown>
2002-12-15 13:09:11 +0000

    doc updates o remove project-map o remove outdated proposals
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/turbine/core/trunk@220802 13f79535-47bb-0310-9956-ffa450edef68

--- xdocs/project-map/update-classlist.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-find ../../src/java/* -type f -name \*java | 
-    sed -e "s/^\.\.\/\.\.\/src\/java\///" |
-    sed -e "s/\.java//" | 
-    sed -e "s/\//./g" > classlist
-
-cvs diff -u classlist > changes  
