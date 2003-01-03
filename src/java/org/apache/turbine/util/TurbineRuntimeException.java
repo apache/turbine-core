75e5590a5ac84222bc88fa8f1765aa9cbb2cee54
Quinton McCombs <quintonm@apache.org>
2003-01-03 07:01:35 +0000

    Now extends NestableRuntimeException from commons-lang
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/turbine/core/trunk@220839 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/turbine/util/TurbineRuntimeException.java
+++ src/java/org/apache/turbine/util/TurbineRuntimeException.java
@@ -54,11 +54,7 @@ package org.apache.turbine.util;
  * <http://www.apache.org/>.
  */
 
-import java.io.PrintStream;
-import java.io.PrintWriter;
-import java.io.StringWriter;
-import java.util.LinkedList;
-import java.util.StringTokenizer;
+import org.apache.commons.lang.exception.NestableRuntimeException;
 
 /**
  * This is a base class of runtime exeptions thrown by Turbine.
@@ -67,26 +63,19 @@ import java.util.StringTokenizer;
  * {@see java.lang.RuntimeException}). It has the nested stack trace
  * functionality found in the {@see TurbineException} class.
  *
- * It's sad that this class is a straight copy/paste of Turbine exception.
- * I wish that Java supported NonCheckedException marker interface...
  *
  * @author <a href="mailto:Rafal.Krzewski@e-point.pl">Rafal Krzewski</a>
+ * @author <a href="mailto:dlr@finemaltcoding.com">Daniel Rall</a>
+ * @author <a href="mailto:quintonm@bellsouth.net">Quinton McCombs</a>
  */
-public class TurbineRuntimeException extends RuntimeException
+public class TurbineRuntimeException extends NestableRuntimeException
 {
-    /**
-     * Holds the reference to the exception or error that caused
-     * this exception to be thrown.
-     */
-    private Throwable nested = null;
-
     /**
      * Constructs a new <code>TurbineRuntimeException</code> without specified
      * detail message.
      */
     public TurbineRuntimeException()
     {
-        super();
     }
 
     /**
@@ -109,8 +98,7 @@ public class TurbineRuntimeException extends RuntimeException
      */
     public TurbineRuntimeException(Throwable nested)
     {
-        super();
-        this.nested = nested;
+        super(nested);
     }
 
     /**
@@ -123,129 +111,7 @@ public class TurbineRuntimeException extends RuntimeException
      */
     public TurbineRuntimeException(String msg, Throwable nested)
     {
-        super(msg);
-        this.nested = nested;
+        super(msg, nested);
     }
 
-    /**
-     * Prints the stack trace of this exception the the standar error
-     * stream.
-     */
-    public void printStackTrace()
-    {
-        synchronized(System.err)
-        {
-            printStackTrace(System.err);
-        }
-    }
-
-    /**
-     * Prints the stack trace of this exception to the specified print stream.
-     *
-     * @param out <code>PrintStream</code> to use for output
-     */
-    public void printStackTrace(PrintStream out)
-    {
-        synchronized(out)
-        {
-            PrintWriter pw=new PrintWriter(out, false);
-            printStackTrace(pw);
-            // flush the PrintWriter before it's GCed
-            pw.flush();
-        }
-    }
-
-    /**
-     * Prints the stack trace of this exception to the specified print writer.
-     *
-     * @param out <code>PrintWriter</code> to use for output.
-     */
-    public void printStackTrace(PrintWriter out)
-    {
-        synchronized(out)
-        {
-            printStackTrace(out, 0);
-        }
-    }
-
-    /**
-     * Prints the stack trace of this exception skiping a specified number
-     * of stack frames.
-     *
-     * @param out <code>PrintWriter</code> to use for output.
-     * @param skip the numbere of stack frames to skip.
-     */
-    public void printStackTrace(PrintWriter out, int skip)
-    {
-        String[] st = captureStackTrace();
-        if(nested != null)
-        {
-            if(nested instanceof TurbineRuntimeException)
-            {
-                ((TurbineRuntimeException)nested).printStackTrace(out, st.length - 2);
-            }
-            else if(nested instanceof TurbineException)
-            {
-                ((TurbineException)nested).printStackTrace(out, st.length - 2);
-            }
-            else
-            {
-                String[] nst = captureStackTrace(nested);
-                for(int i = 0; i<nst.length - st.length + 2; i++)
-                {
-                    out.println(nst[i]);
-                }
-            }
-            out.print("rethrown as ");
-        }
-        for(int i=0; i<st.length - skip; i++)
-        {
-            out.println(st[i]);
-        }
-    }
-
-    /**
-     * Captures the stack trace associated with this exception.
-     *
-     * @return an array of Strings describing stack frames.
-     */
-    private String[] captureStackTrace()
-    {
-        StringWriter sw = new StringWriter();
-        super.printStackTrace(new PrintWriter(sw, true));
-        return splitStackTrace(sw.getBuffer().toString());
-    }
-
-    /**
-     * Captures the stack trace associated with a <code>Throwable</code>
-     * object.
-     *
-     * @param t the <code>Throwable</code>.
-     * @return an array of Strings describing stack frames.
-     */
-    private String[] captureStackTrace(Throwable t)
-    {
-        StringWriter sw = new StringWriter();
-        t.printStackTrace(new PrintWriter(sw, true));
-        return splitStackTrace(sw.getBuffer().toString());
-    }
-
-    /**
-     * Splits the stack trace given as a newline separated string
-     * into an array of stack frames.
-     *
-     * @param stackTrace the stack trace.
-     * @return an array of Strings describing stack frames.
-     */
-    private String[] splitStackTrace(String stackTrace)
-    {
-        String linebreak = System.getProperty("line.separator");
-        StringTokenizer st = new StringTokenizer(stackTrace, linebreak);
-        LinkedList list = new LinkedList();
-        while(st.hasMoreTokens())
-        {
-            list.add(st.nextToken());
-        }
-        return (String [])list.toArray(new String[] {});
-    }
 }
