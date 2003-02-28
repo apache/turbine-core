1a5e6ca7c587b42cbec81683606d2aba42ac67a2
Henning Schmiedehausen <henning@apache.org>
2003-02-28 14:40:22 +0000

    Removed References to TurbineResources
    PR:
    Obtained from:
    Submitted by:
    Reviewed by:
    
    
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/turbine/core/trunk@221106 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/turbine/modules/actions/InitContextsAction.java
+++ src/java/org/apache/turbine/modules/actions/InitContextsAction.java
@@ -57,19 +57,26 @@ package org.apache.turbine.modules.actions;
 import java.util.Hashtable;
 import java.util.Iterator;
 import java.util.Properties;
+
 import javax.naming.InitialContext;
 
+import org.apache.commons.configuration.Configuration;
+
+import org.apache.turbine.Turbine;
+
 import org.apache.turbine.modules.Action;
-import org.apache.turbine.services.resources.TurbineResources;
+
 import org.apache.turbine.util.RunData;
 
 /**
  * Used to initialize JNDI contexts.
  *
  * @author <a href="mailto:greg@shwoop.com">Greg Ritter</a>
+ * @author <a href="mailto:hps@intermeta.de">Henning P. Schmiedehausen</a>
  * @version $Id$
  */
-public class InitContextsAction extends Action
+public class InitContextsAction
+    extends Action
 {
     /**
      * This action will place the contexts defined in the
@@ -82,6 +89,8 @@ public class InitContextsAction extends Action
     public void doPerform(RunData data)
             throws Exception
     {
+        Configuration conf = Turbine.getConfiguration();
+
         // Context properties are specified in lines in the properties
         // file that begin with "context.contextname.", allowing
         // multiple named contexts to be used.  Everything after the
@@ -90,7 +99,7 @@ public class InitContextsAction extends Action
         // instance.
 
         Hashtable contextPropsList = new Hashtable();
-        for (Iterator contextKeys = TurbineResources.getKeys("context."); contextKeys.hasNext();)
+        for (Iterator contextKeys = conf.getKeys("context."); contextKeys.hasNext();)
         {
             String key = (String) contextKeys.next();
             int start = key.indexOf(".") + 1;
@@ -107,7 +116,7 @@ public class InitContextsAction extends Action
                 contextProps = new Properties();
             }
             contextProps.put(key.substring(end + 1),
-                    TurbineResources.getString(key));
+                             conf.getString(key));
             contextPropsList.put(contextName, contextProps);
         }
         for (Iterator contextPropsKeys = contextPropsList.keySet().iterator(); contextPropsKeys.hasNext();)
