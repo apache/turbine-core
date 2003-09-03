3c94ae226172534b91fd24257f5ea3cf15a04b98
Henning Schmiedehausen <henning@apache.org>
2003-09-03 09:14:40 +0000

    Some small improvements on the doctype code from Scott.
    - methods don't need to be static, this is an application tool, so
      it is always referenced from $<toolname>
    
    - provide default values for the various doctype keys; if you don't
      want to have a doctype, either don't use the $page.DocType statement
      on the template or explicitly null out the parameters.
    
    - call private method "buildDocType" to avoid confusion with public
      visible getters
    
    PR:
    Obtained from:
    Submitted by:
    Reviewed by:
    
    
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/turbine/core/trunk@221666 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/turbine/TurbineConstants.java
+++ src/java/org/apache/turbine/TurbineConstants.java
@@ -262,12 +262,25 @@ public interface TurbineConstants
     String DEFAULT_HTML_DOCTYPE_ROOT_ELEMENT_KEY 
             = "default.html.doctype.root.element";
 
+    /** Default value for the doctype root element */
+    String DEFAULT_HTML_DOCTYPE_ROOT_ELEMENT_DEFAULT
+            = "HTML";
+
     /** Default doctype dtd. */
     String DEFAULT_HTML_DOCTYPE_IDENTIFIER_KEY 
             = "default.html.doctype.identifier";
 
+    /** Default Doctype dtd value */
+    String DEFAULT_HTML_DOCTYPE_IDENTIFIER_DEFAULT
+            = "-//W3C//DTD HTML 4.01 Transitional//EN";
+
     /** Default doctype url. */
-    String DEFAULT_HTML_DOCTYPE_URI_KEY = "default.html.doctype.url";
+    String DEFAULT_HTML_DOCTYPE_URI_KEY 
+            = "default.html.doctype.url";
+
+    /** Default doctype url value. */
+    String DEFAULT_HTML_DOCTYPE_URI_DEFAULT
+            = "http://www.w3.org/TR/1999/REC-html401-19991224/loose.dtd";
 
     /** Default Language property */
     String LOCALE_DEFAULT_LANGUAGE_KEY = "locale.default.language";
