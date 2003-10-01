8df24f5b698720dbd501d8b23f4f2dda074fe518
Scott Eade <seade@apache.org>
2003-10-01 04:35:10 +0000

    Ensure an NPE does not occur when emptyValue is used to set an object's primitive properties.
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/turbine/core/branches/TURBINE_2_3_BRANCH@221744 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/turbine/services/intake/model/IntegerField.java
+++ src/java/org/apache/turbine/services/intake/model/IntegerField.java
@@ -121,6 +121,21 @@ public class IntegerField
     }
 
     /**
+     * Provides access to emptyValue such that the value returned will be 
+     * acceptable as an argument parameter to Method.invoke.  Subclasses
+     * that deal with primitive types should ensure that they return an 
+     * appropriate value wrapped in the object wrapper class for the 
+     * primitive type.
+     *   
+     * @return the value to use when the field is empty or an Object that
+     * wraps the empty value for primitive types.
+     */
+    Object getSafeEmptyValue()
+    {
+        return (null == getEmptyValue()) ? new Integer(0) : getEmptyValue(); 
+    }
+
+    /**
      * A suitable validator.
      *
      * @return A suitable validator
