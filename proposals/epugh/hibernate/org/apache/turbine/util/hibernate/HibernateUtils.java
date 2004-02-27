b76c0cb8bd871903fd34ff559fe31222f0fa2853
Scott Eade <seade@apache.org>
2004-02-27 05:12:43 +0000

    Update to use the ASL 2.0 License, remove author tags. Only java files have been dealt with by these commits. Thanks to Jeff Painter for the patch.
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/turbine/core/branches/TURBINE_2_3_BRANCH@221820 13f79535-47bb-0310-9956-ffa450edef68

--- proposals/epugh/hibernate/org/apache/turbine/util/hibernate/HibernateUtils.java
+++ proposals/epugh/hibernate/org/apache/turbine/util/hibernate/HibernateUtils.java
@@ -1,4 +1,20 @@
 package org.apache.turbine.util.hibernate;
+ 
+/*
+ * Copyright 2001-2004 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 import net.sf.hibernate.HibernateException;
 import net.sf.hibernate.JDBCException;
@@ -12,6 +28,8 @@ import org.apache.commons.logging.LogFactory;
  * This class is used to get Hibernate Sessions and may
  * also contain methods (in the future) to get DBConnections
  * or Transactions from JNDI.
+ * 
+ * @version $Id$
  */
 public class HibernateUtils
 {
