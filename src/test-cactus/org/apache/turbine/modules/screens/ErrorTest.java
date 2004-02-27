8346d00058b95f48542519792b89efb174a9c350
Scott Eade <seade@apache.org>
2004-02-27 11:18:45 +0000

    Update to use the ASL 2.0 License, remove author tags. Only java files have been dealt with by these commits. Thanks to Jeff Painter for the patch.
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/turbine/core/branches/TURBINE_2_3_BRANCH@221829 13f79535-47bb-0310-9956-ffa450edef68

--- src/test-cactus/org/apache/turbine/modules/screens/ErrorTest.java
+++ src/test-cactus/org/apache/turbine/modules/screens/ErrorTest.java
@@ -1,4 +1,20 @@
 package org.apache.turbine.modules.screens;
+ 
+/*
+ * Copyright 2001-2004 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
@@ -13,10 +29,7 @@ import org.apache.turbine.util.RunData;
 /**
  *  ErrorTest
  *
- *@author     <a href="epugh@upstate.com">Eric Pugh</a>
- *@created    December 10, 2002
- *@version    $Id: ErrorTest.java,v 1.3 2002/11/08
- *      10:04:13 raphael Exp $
+ *@version    $Id$
  */
 
 public class ErrorTest extends ServletTestCase
