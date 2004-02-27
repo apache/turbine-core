da4e78520789a031568c0f07077fec311eddd3b2
Scott Eade <seade@apache.org>
2004-02-27 05:12:43 +0000

    Update to use the ASL 2.0 License, remove author tags. Only java files have been dealt with by these commits. Thanks to Jeff Painter for the patch.
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/turbine/core/branches/TURBINE_2_3_BRANCH@221820 13f79535-47bb-0310-9956-ffa450edef68

--- proposals/epugh/hibernate/org/apache/turbine/util/hibernate/HibernateFilter.java
+++ proposals/epugh/hibernate/org/apache/turbine/util/hibernate/HibernateFilter.java
@@ -1,4 +1,20 @@
 package org.apache.turbine.util.hibernate;
+ 
+/*
+ * Copyright 2001-2004 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 import java.io.IOException;
 
@@ -17,9 +33,11 @@ import net.sf.hibernate.Session;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
-
-
-
+/**
+ * HibernateFilter
+ *
+ * @version $Id$
+ */
 public class HibernateFilter implements Filter
 {
     //~ Static fields/initializers =============================================
