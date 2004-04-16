85dbd978d14e9c4e55011200efc99647a6a8c371
Scott Eade <seade@apache.org>
2004-04-16 14:22:17 +0000

    Minor changes to sync with my stable version.
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/turbine/core/trunk@221856 13f79535-47bb-0310-9956-ffa450edef68

--- proposals/seade/UIService/org/apache/turbine/services/ui/TurbineUIService.java
+++ proposals/seade/UIService/org/apache/turbine/services/ui/TurbineUIService.java
@@ -1,57 +1,19 @@
 package org.apache.turbine.services.ui;
 
-/* ====================================================================
- * The Apache Software License, Version 1.1
+/*
+ * Copyright 2003-2004 The Apache Software Foundation.
  *
- * Copyright (c) 2001-2003 The Apache Software Foundation.  All rights
- * reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in
- *    the documentation and/or other materials provided with the
- *    distribution.
- *
- * 3. The end-user documentation included with the redistribution,
- *    if any, must include the following acknowledgment:
- *       "This product includes software developed by the
- *        Apache Software Foundation (http://www.apache.org/)."
- *    Alternately, this acknowledgment may appear in the software itself,
- *    if and wherever such third-party acknowledgments normally appear.
- *
- * 4. The names "Apache" and "Apache Software Foundation" and
- *    "Apache Turbine" must not be used to endorse or promote products
- *    derived from this software without prior written permission. For
- *    written permission, please contact apache@apache.org.
- *
- * 5. Products derived from this software may not be called "Apache",
- *    "Apache Turbine", nor may "Apache" appear in their name, without
- *    prior written permission of the Apache Software Foundation.
- *
- * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
- * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
- * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
- * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
- * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- * ====================================================================
- *
- * This software consists of voluntary contributions made by many
- * individuals on behalf of the Apache Software Foundation.  For more
- * information on the Apache Software Foundation, please see
- * <http://www.apache.org/>.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 import java.io.File;
@@ -74,7 +36,6 @@ import org.apache.turbine.util.uri.DataURI;
  * classes and action code. UITool is provided as a pull tool for accessing 
  * skin properties from your templates. 
  * 
- * @author <a href="mailto:seade@backstagetech.com.au">Scott Eade</a>
  * @version $Id$
  * @see UIService
  * @see UITool
@@ -104,28 +65,24 @@ public class TurbineUIService
         org.apache.commons.logging.LogFactory.getLog(TurbineUIService.class);
 
     /**
-     * The location of the skins within the application
-     * resources directory.
+     * The location of the skins within the application resources directory.
      */
     private static final String SKINS_DIRECTORY = "/ui/skins";
 
     /**
-     * The name of the directory where images are
-     * stored for this skin.
+     * The name of the directory where images are stored for this skin.
      */
     private static final String IMAGES_DIRECTORY = "/images";
 
     /**
-     * Property tag for the skin that is to be
-     * used for the web application.
+     * Property tag for the skin that is to be used for the web application.
      */
-    private static final String SKIN_PROPERTY = "tool.ui.skin";
+    private static final String SKIN_PROPERTY_KEY = "tool.ui.skin";
 
     /**
-     * Default skin name. This name actually represents
-     * a directory in the WEBAPP/resources/ui/skins
-     * directory. There is a file called skin.props
-     * which actually contains the name/value pairs.
+     * Default skin name. This name refers to a directory in the 
+     * WEBAPP/resources/ui/skins directory. There is a file called skin.props
+     * which contains the name/value pairs to be made available via the skin.
      */
     public static final String SKIN_PROPERTY_DEFAULT = "default";
 
@@ -137,8 +94,8 @@ public class TurbineUIService
                     + SKINS_DIRECTORY;
 
     /**
-     * The file within the skin directory that actually
-     * contains the name/value pairs for the skin.
+     * The file within the skin directory that actually contains the name/value 
+     * pairs for the skin.
      */
     private static final String SKIN_PROPS_FILE = "skin.props";
 
@@ -334,7 +291,7 @@ public class TurbineUIService
     public String getWebappSkinName()
     {
         return Turbine.getConfiguration()
-                .getString(SKIN_PROPERTY, SKIN_PROPERTY_DEFAULT);
+                .getString(SKIN_PROPERTY_KEY, SKIN_PROPERTY_DEFAULT);
     }
 
     /**
