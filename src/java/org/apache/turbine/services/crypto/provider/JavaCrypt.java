45ee27110a642afc8b5ebf979a3f6bec2b48f2b1
David Eric Pugh <epugh@apache.org>
2004-12-03 10:51:39 +0000

    Fix compile errors with commons-code-1.3
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/turbine/core/trunk@222042 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/turbine/services/crypto/provider/JavaCrypt.java
+++ src/java/org/apache/turbine/services/crypto/provider/JavaCrypt.java
@@ -56,8 +56,8 @@ package org.apache.turbine.services.crypto.provider;
 
 import java.security.MessageDigest;
 
-import org.apache.commons.codec.base64.Base64;
 
+import org.apache.commons.codec.binary.Base64;
 import org.apache.turbine.services.crypto.CryptoAlgorithm;
 
 /**
@@ -149,7 +149,7 @@ public class JavaCrypt
         byte[] digest = md.digest(value.getBytes("UTF-8"));
 
         // Base64-encode the digest.
-        byte[] encodedDigest = Base64.encode(digest);
+        byte[] encodedDigest = Base64.encodeBase64(digest);
         return (encodedDigest == null ? null : new String(encodedDigest));
     }
 }
