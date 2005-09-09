b541180994129067baf7ca62540b7b4197561738
Henning Schmiedehausen <henning@apache.org>
2005-09-09 13:57:46 +0000

    Add unit tests for the BaseValue and DefaultParameter Parser.
    Start to work on that character encoding mess that seems to encumber
    the BaseValueParser.
    
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/turbine/core/branches/TURBINE_2_3_BRANCH@279777 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/org/apache/turbine/util/parser/PropertyBean.java
@@ -0,0 +1,124 @@
+package org.apache.turbine.util.parser;
+
+public class PropertyBean
+{
+    private int intValue;
+
+    private long longValue;
+
+    private double doubleValue;
+
+    private String stringValue;
+
+    private Boolean booleanValue;
+
+    private String doNotTouchValue;
+
+    /**
+     * Get the LongValue value.
+     * @return the LongValue value.
+     */
+    public long getLongValue()
+    {
+        return longValue;
+    }
+
+    /**
+     * Set the LongValue value.
+     * @param newLongValue The new LongValue value.
+     */
+    public void setLongValue(long newLongValue)
+    {
+        this.longValue = newLongValue;
+    }
+    /**
+     * Get the DoubleValue value.
+     * @return the DoubleValue value.
+     */
+    public double getDoubleValue()
+    {
+        return doubleValue;
+    }
+
+    /**
+     * Set the DoubleValue value.
+     * @param newDoubleValue The new DoubleValue value.
+     */
+    public void setDoubleValue(double newDoubleValue)
+    {
+        this.doubleValue = newDoubleValue;
+    }
+
+    /**
+     * Get the StringValue value.
+     * @return the StringValue value.
+     */
+    public String getStringValue()
+    {
+        return stringValue;
+    }
+
+    /**
+     * Set the StringValue value.
+     * @param newStringValue The new StringValue value.
+     */
+    public void setStringValue(String newStringValue)
+    {
+        this.stringValue = newStringValue;
+    }
+
+    /**
+     * Get the IntValue value.
+     * @return the IntValue value.
+     */
+    public int getIntValue()
+    {
+        return intValue;
+    }
+
+    /**
+     * Set the BooleanValue value.
+     * @param newBooleanValue The new BooleanValue value.
+     */
+    public void setBooleanValue(Boolean newBooleanValue)
+    {
+        this.booleanValue = newBooleanValue;
+    }
+
+    /**
+     * Get the BooleanValue value.
+     * @return the BooleanValue value.
+     */
+    public Boolean getBooleanValue()
+    {
+        return booleanValue;
+    }
+
+    /**
+     * Set the IntValue value.
+     * @param newIntValue The new IntValue value.
+     */
+    public void setIntValue(int newIntValue)
+    {
+        this.intValue = newIntValue;
+    }
+
+    /**
+     * Get the DoNotTouchValue value.
+     * @return the DoNotTouchValue value.
+     */
+    public String getDoNotTouchValue()
+    {
+        return doNotTouchValue;
+    }
+
+    /**
+     * Set the DoNotTouchValue value.
+     * @param newDoNotTouchValue The new DoNotTouchValue value.
+     */
+    public void setDoNotTouchValue(String newDoNotTouchValue)
+    {
+        this.doNotTouchValue = newDoNotTouchValue;
+    }
+
+}
