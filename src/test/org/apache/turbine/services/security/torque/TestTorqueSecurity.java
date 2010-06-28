e58ca33f6eced589e63b0ad530b75e8a737d96e4
Thomas Vandahl <tv@apache.org>
2010-06-28 18:58:20 +0000

    Copy Torque-SecurityService to trunk (temporary)
    git-svn-id: https://svn.apache.org/repos/asf/turbine/core/trunk@958682 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/org/apache/turbine/services/security/torque/TestTorqueSecurity.java
@@ -0,0 +1,60 @@
+package org.apache.turbine.services.security.torque;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+import org.apache.turbine.Turbine;
+import org.apache.turbine.services.security.SecurityService;
+import org.apache.turbine.services.security.TurbineSecurity;
+
+import org.apache.turbine.test.HsqlDB;
+
+import org.apache.turbine.test.BaseTurbineTest;
+
+public class TestTorqueSecurity
+        extends BaseTurbineTest
+{
+    public static final String DATA_SOURCE="turbine";
+
+    private HsqlDB hsqlDB = null;
+
+    public TestTorqueSecurity(String name)
+            throws Exception
+    {
+        super(name, "conf/test/TurbineResources.properties");
+        hsqlDB = new HsqlDB("jdbc:hsqldb:.", Turbine.getRealPath("conf/test/create-db.sql"));
+    }
+
+    public static Test suite()
+    {
+        return new TestSuite(TestTorqueSecurity.class);
+    }
+
+    public void testInit()
+    {
+        SecurityService ss = TurbineSecurity.getService();
+
+        assertEquals("No Torque Security Service", ss.getClass(), TorqueSecurityService.class);
+        assertEquals("No Torque User Manager", ss.getUserManager().getClass(), TorqueUserManager.class);
+        assertTrue("Service failed to initialize", ss.getInit());
+    }
+}
