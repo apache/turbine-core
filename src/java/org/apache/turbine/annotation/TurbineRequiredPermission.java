4682d4951d4532b0e1e4dee2b23b35ca07cc3cb9
Thomas Vandahl <tv@apache.org>
2016-11-21 15:21:42 +0000

    First proposal for declarative security
    git-svn-id: https://svn.apache.org/repos/asf/turbine/core/trunk@1770698 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/turbine/annotation/TurbineRequiredPermission.java
@@ -0,0 +1,47 @@
+package org.apache.turbine.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+/**
+ * Annotation to mark modules or methods in modules that require certain permissions to be executed
+ */
+@Retention( RetentionPolicy.RUNTIME )
+@Target( { ElementType.TYPE, ElementType.METHOD } )
+public @interface TurbineRequiredPermission
+{
+    /**
+     * Get the names of the permissions required to use this class or method
+     *
+     * @return an array of permission names
+     */
+    String[] value();
+
+    /**
+     * Get the name of the permission-associated group
+     * 
+     * @return a group name, empty string if the global group
+     */
+    String group() default "";
+}
