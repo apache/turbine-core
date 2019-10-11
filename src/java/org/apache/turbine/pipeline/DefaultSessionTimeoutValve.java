c407e69d59bf80b3962f0307f01927d54280086f
gk <gk@unknown>
2019-10-11 11:52:43 +0000

    - update OWASP - add deprecated tag for DefaultSessionTimeoutValve.java
    git-svn-id: https://svn.apache.org/repos/asf/turbine/core/trunk@1868288 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/turbine/pipeline/DefaultSessionTimeoutValve.java
+++ src/java/org/apache/turbine/pipeline/DefaultSessionTimeoutValve.java
@@ -36,7 +36,10 @@ import org.apache.turbine.util.TurbineException;
  * @author <a href="mailto:mpoeschl@marmot.at">Martin Poeschl</a>
  * @author <a href="mailto:peter@courcoux.biz">Peter Courcoux</a>
  * @version $Id$
+ * 
+ * No replacement. Delegate session timeout to the container e.g. in web.xml, which allows to create a session at any place. 
  */
+@Deprecated
 public class DefaultSessionTimeoutValve
     implements Valve
 {
