ae3be0b32d0a90b7b3d0f296fbc0b99da2d1aab6
Georg Kallidis <gk@apache.org>
2020-02-28 16:04:12 +0000

    - update to parent 7-Snapshot - fix HtmlPageAttributesTest
    git-svn-id: https://svn.apache.org/repos/asf/turbine/core/trunk@1874622 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/turbine/util/template/HtmlPageAttributesTest.java
+++ src/test/org/apache/turbine/util/template/HtmlPageAttributesTest.java
@@ -19,30 +19,78 @@ package org.apache.turbine.util.template;
  * under the License.
  */
 
-import static org.junit.Assert.assertEquals;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.apache.turbine.TurbineConstants;
-import org.junit.Test;
+import org.apache.turbine.annotation.AnnotationProcessor;
+import org.apache.turbine.annotation.TurbineConfiguration;
+import org.apache.turbine.test.BaseTestCase;
+import org.apache.turbine.util.TurbineConfig;
+import org.apache.turbine.util.TurbineException;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 /**
  * Testing of the HtmlPageAttributes class
  *
  * @author <a href="mailto:tv@apache.org">Thomas Vandahl</a>
  */
-public class HtmlPageAttributesTest
+public class HtmlPageAttributesTest extends BaseTestCase
 {
-    @Test public void testBuildDoctype()
+    
+    @TurbineConfiguration( TurbineConstants.DEFAULT_HTML_DOCTYPE_ROOT_ELEMENT_KEY )
+    private String defaultHtmlDoctypeRootElement = TurbineConstants.DEFAULT_HTML_DOCTYPE_ROOT_ELEMENT_DEFAULT;
+
+    @TurbineConfiguration( TurbineConstants.DEFAULT_HTML_DOCTYPE_IDENTIFIER_KEY )
+    private String defaultHtmlDoctypeIdentifier;
+
+    @TurbineConfiguration( TurbineConstants.DEFAULT_HTML_DOCTYPE_URI_KEY )
+    private String defaultHtmlDoctypeUri;
+    
+    private static TurbineConfig tc = null;
+
+    @BeforeAll
+    public static void init() throws Exception
+    {
+        tc = new TurbineConfig(".", "/conf/test/CompleteTurbineResources.properties");
+        tc.initialize();
+    }
+
+    @AfterAll
+    public static void destroy()
+        throws Exception
+    {
+        tc.dispose();
+    }
+
+    @BeforeEach
+    public void setUpBefore() throws Exception
+    {
+        // do nothing
+    }
+    
+    @Test public void testBuildDoctype() throws TurbineException
     {
         HtmlPageAttributes page = new HtmlPageAttributes();
         assertEquals("<!DOCTYPE html>", page.getDoctype("html", null, null));
         assertEquals("<!DOCTYPE html>", page.getDoctype("html", "", ""));
         assertEquals("<!DOCTYPE html SYSTEM \"bla\">", page.getDoctype("html", "", "bla"));
-
-        assertEquals("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\" "
-                + "\"http://www.w3.org/TR/1999/REC-html401-19991224/loose.dtd\">",
+        
+       // by default empty in HTML 5
+        assertEquals("<!DOCTYPE HTML>",
                 page.getDoctype(TurbineConstants.DEFAULT_HTML_DOCTYPE_ROOT_ELEMENT_DEFAULT,
                         TurbineConstants.DEFAULT_HTML_DOCTYPE_IDENTIFIER_DEFAULT,
                         TurbineConstants.DEFAULT_HTML_DOCTYPE_URI_DEFAULT));
+        // test old style locally
+        AnnotationProcessor.process(this);
+        assertEquals("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\" "
+                + "\"http://www.w3.org/TR/1999/REC-html401-19991224/loose.dtd\">",
+                page.getDoctype(defaultHtmlDoctypeRootElement,
+                                defaultHtmlDoctypeIdentifier,
+                                defaultHtmlDoctypeUri));
 
         // HTML 5 is the default
         assertEquals("<!DOCTYPE HTML>", page.getDefaultDoctype());
